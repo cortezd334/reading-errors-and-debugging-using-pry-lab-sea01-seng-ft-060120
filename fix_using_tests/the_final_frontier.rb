2ac52b20ef6a85c769ac18edade17b217cbc6648
Danira Cortez <cortezd334@gmail.com>
2020-05-09 05:41:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,13 +4,13 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-def state_log(generate_star_date)
-  "Captain's Log, star date #{generate_star_date}."
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
 end
 
 def engage
   #puts date
   date = generate_star_date
-  state_log(date.to_s)
+  state_log(date)
 end
 #binding.pry
